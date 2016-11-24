3bc0be2cbe2a2109ec0616602a94d03b3adab60f
Bhupeshraj Senthilkumar <bhupeshrajs@gmail.com>
2016-11-24 01:27:13 -0500

    Checking for less input

--- Mandelbrot/mandelbrot.cu
+++ Mandelbrot/mandelbrot.cu
@@ -79,6 +79,8 @@ __global__ void mandelbrotCUDA(
     int row = blockIdx.y * blockDim.y + threadIdx.y; // WIDTH
     int col = blockIdx.x * blockDim.x + threadIdx.x; // HEIGHT
     
+    printf("\n row and column are : (%d,%d)",row,column) ;
+    
     int index = row * (*d_width) + col;
     
     if( col >= (*d_width) ) return;
@@ -122,8 +124,8 @@ int main(int argc, char *argv[])
     
     
     /* Height , width of the image */
-    const int width = 1200;
-    const int height = 800;
+    const int width = 12;
+    const int height = 8;
     
     /* Max iterations to do */
     const int maxIterations = 256;
